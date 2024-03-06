1b8e5923e22ca797582a9cd2860461e90425c139
Yuri Lima <guilherme.yurinhu@gmail.com>
2024-03-06 16:23:00 -0300

    Total da soma

---
+++ TotalDaSoma/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int INDICE = 13, SOMA = 0, K = 0;
+
+        while (K < INDICE)
+        {
+            K = K + 1;
+            SOMA = SOMA + K;
+        }
+
+        Console.WriteLine(SOMA);
+    }
+}
