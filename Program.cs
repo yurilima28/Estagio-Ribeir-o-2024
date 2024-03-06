8876b51e0e4ed9601822b9ba6b21b380f434e2c4
Yuri Lima <guilherme.yurinhu@gmail.com>
2024-03-06 15:15:01 -0300

    Sequencia de fibonacci

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Informe um número: ");
+        int numeroInformado = int.Parse(Console.ReadLine());
+
+        if (PertenceSequenciaFibonacci(numeroInformado))
+        {
+            Console.WriteLine($"O número {numeroInformado} pertence à sequência de Fibonacci.");
+        }
+        else
+        {
+            Console.WriteLine($"O número {numeroInformado} NÃO pertence à sequência de Fibonacci.");
+        }
+    }
+
+    static bool PertenceSequenciaFibonacci(int numero)
+    {
+        int a = 0, b = 1;
+
+        while (a <= numero)
+        {
+            if (a == numero)
+            {
+                return true;
+            }
+
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+
+        return false;
+    }
+}
